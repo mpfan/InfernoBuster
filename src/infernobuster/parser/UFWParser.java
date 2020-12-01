522cc589792698693a7749e4fb6e95ad845f9aa5
Michael <mfan20107@gmail.com>
2020-12-01 16:23:00 -0500

    feat: add parser superclass

--- src/infernobuster/parser/UFWParser.java
+++ src/infernobuster/parser/UFWParser.java
@@ -1,10 +1,20 @@
 package infernobuster.parser;
 
+import java.io.File;
+import java.util.ArrayList;
+
 /*
  * -A ufw-user-output -p tcp -d 10.0.2.5 –dport 55 -s 10.0.2.4 –sport 45 -j DROP
  *
  */
 
-public class UFWParser {
+public class UFWParser extends Parser{
+	public UFWParser() {}
+
+	public ArrayList<Rule> parse(File file) {
+		
+		
+		return null;
+	}
 	
 }
