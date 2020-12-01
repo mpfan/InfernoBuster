5b0ea52fd1c35e8675742591d9f348e75770e2b0
Michael <mfan20107@gmail.com>
2020-12-01 16:23:00 -0500

    feat: add parser superclass

--- src/infernobuster/parser/Parser.java
+++ src/infernobuster/parser/Parser.java
@@ -1,7 +1,28 @@
 package infernobuster.parser;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 
-public interface Parser {
-	public Rule[] parse(File file);
+public abstract class Parser {
+	protected String read(String filename) {
+		File file = new File(filename); 
+		  
+		BufferedReader br;
+		StringBuilder sb = null;
+		try {
+			br = new BufferedReader(new FileReader(file));
+			
+			sb = new StringBuilder();
+			String line;
+			while ((line = br.readLine()) != null) {
+				sb.append(line);
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+		
+		return sb.toString();
+	}
 }
